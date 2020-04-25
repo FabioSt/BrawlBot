f50faa911f367cc5e1186d76d3305352598880b8
Fabio Staiano <fabio.staiano@gmail.com>
2020-04-25 21:58:52 +0200

    initial

---
+++ Assets/Scripts/BtnStart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BtnStart : MonoBehaviour
+{
+	public Button startBtn;
+
+	void Start()
+	{
+		Button btn = startBtn.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+        StartCoroutine(ButtonDelay());
+        
+	}
+
+
+    IEnumerator ButtonDelay()
+    {
+        yield return new WaitForSeconds(1f);
+
+        // This line will be executed after x seconds passed
+        SceneManager.LoadScene("Arena");
+    }
+
+}
